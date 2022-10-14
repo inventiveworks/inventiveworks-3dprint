10df883975be80680ba0470a62d1946436f51625
inventiveworks <inventiveworks2@gmail.com>
2022-10-14 16:55:44 +0200

    1

---
+++ Admin/password/pass.js
@@ -0,0 +1,12 @@
+var password = "!Admin@!"
+function passcheck(){
+
+    if(document.getElementById('pass1').value != password){
+        alert('Wrong Password, Try Again');
+        return false;
+    }
+    if(document.getElementById('pass1').value == password){
+        alert('Hello Admin... Nice To See You!');
+    }
+
+}
