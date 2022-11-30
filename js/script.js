5ca6aee1874a99696190b077eb6facb5f558a84f
inventiveworks <inventiveworks2@gmail.com>
2022-11-30 10:31:46 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,80 +1,68 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
 
 
-$(document).ready(function(){
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
 
-    // wow initiation
-    new WOW().init();
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
 
-    // navigation bar toggle
-    $('#navbar-toggler').click(function(){
-        $('.navbar-collapse').slideToggle(400);
-    });
-
-    // navbar bg change on scroll
-    $(window).scroll(function(){
-        let pos = $(window).scrollTop();
-        if(pos >= 100){
-            $('.navbar').addClass('cng-navbar');
-        } else {
-            $('.navbar').removeClass('cng-navbar');
-        }
-    });
-
-    // sample video popup
-    $(document).ready(function() {
-        $('.popup-youtube').magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: false,
-    
-            fixedContentPos: false
-        });
-    });
-
-    // team carousel 
-    $('.team .owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        autoplay: true,
-        dots: true,
-        nav: false,
-        responsiveClass: true,
-        responsive:{
-            0:{
-                items: 1
-            }, 
-            600:{
-                items: 2
-            },
-            1000:{
-                items: 3
-            }
-        }
-    });
-
-    // faq accordion
-    $('.faq-head').each(function(){
-        $(this).click(function(){
-            $(this).next().toggleClass('show-faq-content');
-            let icon = $(this).children('span').children("i").attr('class');
-
-            if(icon == "fas fa-plus"){
-                $(this).children('span').html('<i class = "fas fa-minus"></i>');
-            } else {
-                $(this).children('span').html('<i class = "fas fa-plus"></i>');
-            }
-        });
-    });
-
-    // testimonial carousel 
-    $('.testimonial .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        nav: false,
-        items: 1
-    });
-
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
 });
